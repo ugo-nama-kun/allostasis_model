84cfd6b1a34f44314e0e9ef6e1bb0e57f80a49ee
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2023-09-21 02:59:58 +0900

    refine plot

--- refine_plot.py
+++ refine_plot.py
@@ -18,14 +18,14 @@ x_range = [250, 425]
 plt.figure(figsize=(8, 4), dpi=100)
 
 plt.subplot(3, 1, 1)
+plt.plot(data["cue"][x_range[0]:x_range[1]], "r", alpha=1)
+plt.plot(data["memory"][x_range[0]:x_range[1]], "blue", alpha=0.3)
+plt.legend(["cue", "memory"])
+
+plt.subplot(3, 1, 2)
 plt.plot(data["temp"][x_range[0]:x_range[1]], alpha=0.5)
 plt.plot(np.arange(x_range[1]- x_range[0]), TEMP_SETPOINT * np.ones_like(data["temp"][x_range[0]:x_range[1]]), "--k", alpha=0.5)
 plt.legend(["temperature"])
-
-plt.subplot(3, 1, 2)
-plt.plot(data["cue"][x_range[0]:x_range[1]], "r", alpha=1)
-plt.plot(data["memory"][x_range[0]:x_range[1]], "blue", alpha=0.3)
-plt.legend(["cue-presentation", "memory"])
 
 plt.subplot(3, 1, 3)
 plt.plot(data["load"][x_range[0]:x_range[1]], "r", alpha=0.5)
